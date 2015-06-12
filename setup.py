ff45c176c4cbedd9fdf0845042ce1883ac939373
Filipe Maia <filipe.c.maia@gmail.com>
2015-06-12 14:53:12 +0200

    Fix ARCH problems

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ setup.py file for SWIG example
 """
 
 from distutils.core import setup, Extension
+import os
 import numpy
 
+os.environ["ARCHFLAGS"]="-arch x86_64" 
 arrayfire_module = Extension('_arrayfire',
                              sources=['afnumpy/af_wrap.cxx'],
                              libraries = ['afcuda'],
                              runtime_library_dirs = ['/usr/local/lib'],
                              extra_link_args = ['-Wl,-rpath,/usr/local/lib'],
+                             extra_compile_args = ['-std=c++11','-g','-O0','-fno-omit-frame-pointer'],
                          )
 
 setup (name = 'afnumpy',
