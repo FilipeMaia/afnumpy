c88dbe3f6bf099a3bae9594770a8382cdc3ba31a
Filipe Maia <filipe.c.maia@gmail.com>
2015-06-15 17:18:45 +0200

    Initial fft2 implementation and test

--- afnumpy/fft.py
+++ afnumpy/fft.py
@@ -1,17 +1,18 @@
 import numpy
+from afnumpy.multiarray import ndarray
 import arrayfire
-
+import private_utils as pu
 
 def fft2(a, s=None, axes=(-2, -1)):
     if s is not None:
         raise NotImplementedError
-    ft_axes = axes
+    ft_axes = list(axes)
     for i in range(0,2):
         if axes[i] < 0:
             ft_axes[i] = len(a.shape)+axes[i]
         ft_axes[i] = len(a.shape)-ft_axes[i]-1
     s = arrayfire.fft2(a.d_array, ft_axes[0], ft_axes[1])
-    return ndarray(self.shape, dtype=__InvTypeMap__[s.type()], af_array=s)
+    return ndarray(a.shape, dtype=pu.InvTypeMap[s.type()], af_array=s)
 
 
     
