f0b2fc12c825094363fee89019b9b8c18fc351c5
Filipe Maia <filipe.c.maia@gmail.com>
2015-09-08 17:12:40 +0200

    Prepare for pip

--- setup.py
+++ setup.py
@@ -9,8 +9,12 @@ from distutils.core import setup
 
 
 setup (name = 'afnumpy',
-       version = '0.1',
+       version = '1.0a3',
        author      = "Filipe Maia",
+       author_email = "filipe.c.maia@gmail.com",
+       url = 'https://github.com/FilipeMaia/afnumpy',
+       download_url = 'https://github.com/afnumpy/tarball/v1.0a3',
+       keywords = ['arrayfire', 'numpy', 'GPU'],
        description = """A GPU-ready drop-in replacement for numpy""",
        packages = ["afnumpy", "afnumpy/core", "afnumpy/lib", "afnumpy/linalg"],
        )
